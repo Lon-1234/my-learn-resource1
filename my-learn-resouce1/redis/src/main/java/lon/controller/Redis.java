41a31547efa8e3dc6cee79e72ee942c55b9b2026
Long <Long@qq.com>
2026-06-06 19:54:42 +0800

    修改了一个小小的错误

--- my-learn-resouce1/redis/src/main/java/lon/controller/Redis.java
+++ my-learn-resouce1/redis/src/main/java/lon/controller/Redis.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
-import jakarta.annotation.Resource;
+
+import javax.annotation.Resource;
 import java.util.*;
 
 
